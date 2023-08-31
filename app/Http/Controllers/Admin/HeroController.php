09841116ca90099f95045d1a2afc169ed95e7e12
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-08-31 22:17:21 +0800

    [Progress] Hero Section (part - 2)

--- app/Http/Controllers/Admin/HeroController.php
+++ app/Http/Controllers/Admin/HeroController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hero;
 use Illuminate\Http\Request;
 
 class HeroController extends Controller
@@ -52,7 +53,36 @@ class HeroController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'title' => ['required', 'max:200'],
+            'sub_title' => ['required', 'max:500'],
+            'image' => ['max:3000', 'image']
+        ]);
+
+        // dd($request->all());
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = rand() . $image->getClientOriginalName();
+            $image->move(public_path('/uploads'), $imageName);
+            $imagePath = "/uploads/" . $imageName;
+            // dd($imagePath);
+        }
+
+        //Check whether the data exists or not, if not exist it will create and vice versa
+        Hero::updateOrCreate(
+            ['id' => $id],
+            [
+                'title' => $request->title,
+                'sub_title' => $request->sub_title,
+                'btn_text' => $request->btn_text,
+                'btn_url' => $request->btn_url,
+                'image' => isset($imagePath) ? $imagePath : ''
+            ]
+        );
+        toastr()->success('Updated Successfully', 'Hero Section');
+        return redirect()->back();
+
     }
 
     /**
