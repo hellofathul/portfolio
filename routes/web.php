5b3a7e2b7af0e4d8a2686d359af76cb33c0a866c
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-08-31 19:09:46 +0800

    Finish Mastering Front-End Portfolio Details page

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,14 @@ Route::get('/blog', function () {
     return view('frontend.blog');
 });
 
+Route::get('/blog-details', function () {
+    return view('frontend.blog-details');
+});
+
+Route::get('/portfolio-details', function () {
+    return view('frontend.portfolio-details');
+});
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
