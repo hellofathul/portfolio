e2b5b7e354edea80ea6b2c6cd22adaba9a6cd4d1
hellofathul <116142085+hellofathul@users.noreply.github.com>
2023-09-07 03:21:31 +0800

    Portfolio Section (Show dynamic contents in home page)

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\About;
+use App\Models\Category;
 use App\Models\Hero;
+use App\Models\PortfolioItem;
+use App\Models\PortfolioSectionSetting;
 use App\Models\Service;
 use App\Models\TyperTitle;
 use Illuminate\Http\Request;
@@ -12,11 +15,26 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $hero = Hero::first();
         $typerTitles = TyperTitle::all();
         $service = Service::all();
         $about = About::first();
-        return view('frontend.home', compact('hero', 'typerTitles', 'service', 'about'));
+        $portfolioTitle = PortfolioSectionSetting::first();
+        $portfolioCategory = Category::all();
+        $portfolioItems = PortfolioItem::all();
+        return view(
+            'frontend.home',
+            compact(
+                'hero',
+                'typerTitles',
+                'service',
+                'about',
+                'portfolioTitle',
+                'portfolioCategory',
+                'portfolioItems',
+            )
+        );
     }
 }
